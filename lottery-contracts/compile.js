b57f906435802dc7a1cbd37281070388ad9560fc
eduwr <eduwric@gmail.com>
2021-07-19 20:13:18 -0300

    refactor: update libraries to newer version

--- lottery-contracts/compile.js
+++ lottery-contracts/compile.js
@@ -1,8 +1,33 @@
+const solc = require("solc");
 const path = require("path");
 const fs = require("fs");
-const solc = require("solc");
 
-const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
-const source = fs.readFileSync(lotteryPath, "utf8");
+const contractPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(contractPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+const input = {
+  language: "Solidity",
+  sources: {
+    "Lottery.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const {
+  abi,
+  evm: {
+    bytecode: { object: bytecode },
+  },
+} = JSON.parse(solc.compile(JSON.stringify(input))).contracts["Lottery.sol"][
+  "Lottery"
+];
+
+module.exports = { abi, bytecode };
--- lottery-contracts/deploy.js
+++ lottery-contracts/deploy.js
@@ -1,14 +1,12 @@
 const fs = require("fs");
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const { abi, bytecode } = require("./compile");
 
-const provider = new HDWalletProvider(
-  "they front exile forum lottery depart oven install demand trophy shallow wait",
-  // remember to change this to your own phrase!
-  "https://rinkeby.infura.io/v3/35591972e70f47c78fbe00c49ea4b9f7"
-  // remember to change this to your own endpoint!
-);
+const infuraUrl = "INFURA URL";
+const mnemonic = "YOUR MNEMONIC PHRASE";
+
+const provider = new HDWalletProvider(mnemonic, infuraUrl);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -16,15 +14,17 @@ const deploy = async () => {
 
   console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
+  const result = await new web3.eth.Contract(abi)
     .deploy({ data: bytecode })
     .send({ gas: "1000000", from: accounts[0] });
 
   fs.writeFileSync(
     "./abis/lottery.json",
-    JSON.stringify(interface).replace(/\\/g, "")
+    JSON.stringify(abi).replace(/\\/g, "")
   );
-  console.log(interface);
+
+  provider.engine.stop();
+  // console.log(abi);
   console.log("Contract deployed to", result.options.address);
 };
 
